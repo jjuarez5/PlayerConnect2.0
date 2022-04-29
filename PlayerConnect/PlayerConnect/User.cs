cb56c8e2392851b74858bb86087ffeac453d0754
jjuarez5 <julio.juarez5@outlook.com>
2022-04-29 01:01:41 -0500

    add user class and cosmos db package

---
+++ PlayerConnect/PlayerConnect/User.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace PlayerConnect
+{
+    public class User
+    {
+        [JsonProperty(PropertyName="Id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName="UserName")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName ="Friends")]
+        public List<User> Friends { get; set; }
+    }
+}
